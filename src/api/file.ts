8c535abeb8e7df3751c8d929d32979cc85c7801e
白开水浇花 <fengqiyue@gmail.com>
2024-10-24 00:13:54 +0800

    refactor: 重构 ImageGalleryUploader 组件

---
+++ src/api/file.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export interface FileRecord {
+  id: string;
+  name: string;
+  path: string;
+  url: string;
+}
+
+// 列表返回数据
+export interface FilesResponse {
+  data: FileRecord[];
+  meta: {
+    page: number;
+    page_size: number;
+    has_more: boolean;
+    total: number;
+  };
+}
+
+// 图片列表
+export function queryFiles(params) {
+  return axios.get<any, FilesResponse>('/api/file/images', {
+    params,
+  });
+}
+
+// 批量删除文件
+export function deleteFiles(ids: string[]) {
+  return axios.delete(`/api/files`, {
+    data: {
+      file_ids: ids,
+    },
+  });
+}
--- src/components/ImageGalleryUploader/index.vue
+++ src/components/ImageGalleryUploader/index.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
-  import { computed, onMounted, ref } from 'vue';
-  import axios from 'axios';
+  import { computed, onMounted, ref, watch } from 'vue';
+  import { Message } from '@arco-design/web-vue';
   import { FileItem } from '@arco-design/web-vue/es/upload/interfaces';
-  import Item from './item.vue';
-
-  interface ImageItem {
-    id: string;
-    url: string;
-  }
+  import { getToken } from '@/utils/auth';
+  import { FileRecord, deleteFiles, queryFiles } from '@/api/file';
 
   const props = defineProps({
     modelValue: {
       type: [Array, String],
       default: () => [],
-    },
-    action: {
-      type: String,
-      required: true,
     },
     limit: {
       type: Number,
@@ -32,7 +24,7 @@
      *  standard (4:3)：标准屏
      *  photo (3:2)：照片比例
      */
-    mode: {
+    ratio: {
       type: String,
       default: 'square',
     },
@@ -40,30 +32,35 @@
 
   const emits = defineEmits(['update:modelValue']);
 
+  const token = getToken();
+  const action = `${import.meta.env.VITE_API_BASE_URL}/api/upload/image`;
+
   const fileLimit = ref(props.limit);
   const fileList = ref<FileItem[]>([]);
-  const selectKey = ref<string>();
   const selectKeys = ref<string[]>([]);
-  const checkboxDisabled = computed(() => selectKeys.value.length + fileList.value.length >= fileLimit.value);
+  const selectCount = computed(() => selectKeys.value.length);
 
   const show = ref(false);
   const loading = ref(false);
   const current = ref(1);
+  const pageSize = ref(24);
   const total = ref(0);
-  const list = ref<ImageItem[]>([]);
+  const list = ref<FileRecord[]>([]);
   const isEmptyData = computed(() => list.value.length === 0 && !loading.value);
+
+  const deleteLoading = ref(false);
+  const uploadFiles = ref<string[]>([]);
+  const uploadLoading = computed(() => uploadFiles.value.length > 0);
 
   const fetchData = async () => {
     try {
       loading.value = true;
-      const { data } = await axios.get(props.action, {
-        params: {
-          page: current.value,
-          page_size: 18,
-        },
+      const response = await queryFiles({
+        page: current.value,
       });
-      list.value = data.list;
-      total.value = data.total;
+      list.value = response.data;
+      total.value = response.meta.total;
+      pageSize.value = response.meta.page_size;
     } finally {
       loading.value = false;
     }
@@ -79,64 +76,67 @@
     fetchData();
   };
 
-  const onDeleteItem = async (data: ImageItem) => {
+  const onDeleteItems = async () => {
     try {
-      loading.value = true;
-      await axios.delete(props.action, {
-        data: {
-          id: data.id,
-        },
-      });
+      deleteLoading.value = true;
+      await deleteFiles(selectKeys.value);
+      selectKeys.value = [];
       onRefresh();
     } finally {
-      loading.value = false;
+      deleteLoading.value = false;
     }
   };
 
   // 仅用于触发弹窗，它不做其它操作
   const handleButtonClick = () => {
     return new Promise<FileList>((resolve) => {
-      show.value = true; // 显示弹窗
-      fetchData(); // 获取数据
-      // 返回一个空的 FileList
+      show.value = true;
+
+      onRefresh();
+
+      // 返回一个空的 FileList 以阻止后续的上传动作
       resolve(new DataTransfer().files);
     });
   };
 
+  // 取消弹窗
   const handleCancelModal = () => {
     show.value = false;
-  };
-
-  const handleConfirmModal = () => {
-    show.value = false;
-    if (fileLimit.value !== 1) {
-      const newFiles = list.value
-        .filter((item) => selectKeys.value.includes(item.id))
-        .map((item) => ({
-          uid: item.id,
-          url: item.url,
-        }));
-      fileList.value = [...fileList.value, ...newFiles];
-      emits('update:modelValue', fileList.value);
-    } else {
-      const newFile = list.value.find((item) => item.id === selectKey.value);
-      if (newFile) {
-        fileList.value = [
-          {
-            uid: newFile.id,
-            url: newFile.url,
-          },
-        ];
-        emits('update:modelValue', newFile?.url);
-      }
-    }
-  };
-
-  const closeModal = () => {
-    selectKey.value = '';
     selectKeys.value = [];
   };
 
+  // 确认弹窗，返回图片给父组件
+  const handleConfirmModal = () => {
+    show.value = false;
+
+    // 全部返回
+    const filterFiles = list.value.filter((item) => selectKeys.value.includes(item.id));
+    const newFiles = filterFiles.map((item) => ({ uid: item.id, url: item.url }));
+    const allFiles = [...fileList.value, ...newFiles];
+
+    if (fileLimit.value === 1) {
+      // 返回最后 1 张选中的图片
+      const file = allFiles[allFiles.length - 1];
+      fileList.value = [file]; // 为了更改视图
+      emits('update:modelValue', file?.url || '');
+    } else if (fileLimit.value > 1) {
+      // 返回张选中的图片里最后的 fileLimit.value 张
+      const files = allFiles.slice(-fileLimit.value);
+      fileList.value = files; // 为了更改视图
+      emits('update:modelValue', files);
+    } else {
+      // 全部返回
+      fileList.value = allFiles; // 为了更改视图
+      emits('update:modelValue', allFiles);
+    }
+  };
+
+  // 关闭弹窗
+  const closeModal = () => {
+    selectKeys.value = [];
+  };
+
+  // 移除图片
   const beforeRemove = (fileItem: FileItem) => {
     return new Promise<boolean>((resolve) => {
       if (fileLimit.value === 1) {
@@ -149,25 +149,57 @@
     });
   };
 
+  // 上传前
+  const beforeUpload = (file: File) => {
+    return new Promise<File>((resolve) => {
+      uploadFiles.value.push(file.name);
+      resolve(file);
+    });
+  };
+
+  // 上传成功
+  const uploadSuccess = (response?: any) => {
+    const { code, message } = response.response;
+
+    // 移除上传中的文件
+    uploadFiles.value = uploadFiles.value.filter((item) => item !== response.file.name);
+
+    if (code !== 0) {
+      Message.error(message);
+    }
+  };
+
+  // 上传失败
+  const uploadError = (error: Error) => {
+    uploadFiles.value = uploadFiles.value.filter((item) => item !== error.name);
+    Message.error(error.message);
+  };
+
+  // 监控待上传文件列表，当全部上传完成时，刷新列表
+  watch(uploadFiles, (files: string[]) => {
+    if (files.length === 0) {
+      onRefresh();
+    }
+  });
+
   onMounted(() => {
-    // uid 是 <a-uploader /> 的 FileItem 的参数
-    // 我用它来标识图片的唯一性，当初始化的时候，没有就构造了一个
-    // item.key 是考虑到接口有可能会返回它，比如上传到云存储的也会返回一个key
+    // 构造 fileList 的目的是为了给组件展示图片例表使用 <a-uploader />
     if (Array.isArray(props.modelValue)) {
       fileList.value = props.modelValue.map((item, index) => {
         return {
-          uid: item.id || item.key || `uid-${index}-${Date.now()}`,
-          url: item.url || item,
+          uid: item.id || `uid-${index}-${Date.now()}`,
+          url: item.url,
         };
       });
-    } else {
+    } else if (props.modelValue) {
+      // 传入字符串时，表示只能 1 张图
       fileLimit.value = 1;
       fileList.value = [
-        {
-          uid: `uid-${Date.now()}`,
-          url: props.modelValue,
-        },
+        // 构造组件需要的数据结构 FileItem
+        { uid: `uid-${Date.now()}`, url: props.modelValue },
       ];
+    } else {
+      fileList.value = [];
     }
   });
 </script>
@@ -178,70 +210,65 @@
     <!--还好它有个 button-click 可以阻止后续的动作-->
     <a-upload
       v-model:file-list="fileList"
-      :action="action"
       :limit="fileLimit"
-      :class="'m-' + mode"
+      :class="'m-' + ratio"
+      :image-preview="true"
       list-type="picture-card"
       image-loading="lazy"
-      image-preview
-      @button-click="handleButtonClick"
       @before-remove="beforeRemove"
+      @button-click="handleButtonClick"
     />
 
     <!--图片上传、选择弹窗-->
-    <a-modal
-      v-model:visible="show"
-      :mask-closable="false"
-      :modal-style="{ 'width': '100%', 'min-width': '360px', 'max-width': '820px' }"
-      title="图片例表"
-      title-align="start"
-      @close="closeModal"
-    >
-      <div class="toolbar-container">
-        <!--这个按钮才实现了上传功能-->
-        <a-upload
-          :show-file-list="false"
-          :limit="limit"
-          :name="name"
-          accept="image/png, image/jpeg, image/gif"
-          multiple
-        />
-        <a-button @click="onRefresh">
-          <template #icon><icon-refresh /></template>
-        </a-button>
-      </div>
-      <a-spin :loading="loading" class="gallery-container">
-        <a-empty v-if="isEmptyData" style="margin: 0 auto" />
-        <div class="gallery-image-container">
-          <!--使用官方的组件来实现单选和多选-->
-          <a-radio-group v-if="fileLimit === 1" v-model="selectKey">
-            <a-row :gutter="[12, 12]">
-              <a-col v-for="item in list" :key="item.id" :span="4">
-                <a-radio :value="item.id">
-                  <template #radio="{ checked }">
-                    <Item :data="item" :checked="checked" @delete="onDeleteItem" />
-                  </template>
-                </a-radio>
-              </a-col>
-            </a-row>
-          </a-radio-group>
-          <a-checkbox-group v-else v-model="selectKeys" :disabled="checkboxDisabled">
-            <a-row :gutter="[12, 12]">
-              <a-col v-for="item in list" :key="item.id" :span="4">
-                <a-checkbox :value="item.id">
-                  <template #checkbox="{ checked }">
-                    <Item :data="item" :checked="checked" @delete="onDeleteItem" />
-                  </template>
-                </a-checkbox>
-              </a-col>
-            </a-row>
-          </a-checkbox-group>
+    <a-modal v-model:visible="show" :mask-closable="false" width="810px" title-align="start" @close="closeModal">
+      <template #title> 图片例表 </template>
+      <a-space direction="vertical" size="medium" fill>
+        <div class="toolbar-container">
+          <a-space>
+            <!--这个按钮才实现了上传功能-->
+            <a-upload
+              :multiple="true"
+              :show-file-list="false"
+              :limit="limit"
+              :action="action"
+              :headers="{ Authorization: `Bearer ${token}` }"
+              name="image"
+              accept="image/png, image/jpeg, image/gif"
+              @success="uploadSuccess"
+              @error="uploadError"
+              @before-upload="beforeUpload"
+            >
+              <template #upload-button>
+                <a-button :loading="uploadLoading" type="primary">
+                  <template #icon><icon-upload /></template>
+                  上传图片
+                </a-button>
+              </template>
+            </a-upload>
+            <a-button v-if="selectCount" :loading="deleteLoading" type="primary" status="danger" @click="onDeleteItems">
+              删除（{{ selectCount }}）
+            </a-button>
+          </a-space>
+          <a-button @click="onRefresh">
+            <template #icon><icon-refresh /></template>
+          </a-button>
         </div>
-      </a-spin>
-
+        <a-spin :loading="loading" :class="{ 'gallery-container-empty': isEmptyData }" class="gallery-container">
+          <a-empty v-if="isEmptyData" />
+          <a-checkbox-group v-model="selectKeys" type="button">
+            <div class="gallery-image-container">
+              <a-checkbox v-for="item in list" :key="item.id" :value="item.id">
+                <template #checkbox>
+                  <a-image :src="item.url" :preview="false" width="120" height="90" fit="cover" show-loader />
+                </template>
+              </a-checkbox>
+            </div>
+          </a-checkbox-group>
+        </a-spin>
+      </a-space>
       <template #footer>
         <div class="footer-container">
-          <a-pagination :total="total" :current="current" show-total simple @change="onChangePage" />
+          <a-pagination :total="total" :current="current" :page-size="pageSize" show-total @change="onChangePage" />
           <div class="footer-button">
             <a-button @click="handleCancelModal">取消</a-button>
             <a-button type="primary" @click="handleConfirmModal">确定</a-button>
@@ -262,11 +289,36 @@
 
   .gallery-container {
     display: flex;
-    align-items: center;
-    min-height: 400px;
+    min-height: 390px;
+
+    &-empty {
+      align-items: center;
+    }
 
     .gallery-image-container {
-      margin-top: 20px;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 10px;
+      align-content: flex-start;
+      width: 100%;
+      min-height: 390px;
+
+      .arco-radio,
+      .arco-checkbox {
+        margin-right: 0;
+        padding-left: 0;
+      }
+
+      .arco-image {
+        overflow: hidden;
+        border: 2px solid var(--color-neutral-1);
+      }
+
+      .arco-checkbox-checked {
+        .arco-image {
+          border-color: rgb(var(--primary-6));
+        }
+      }
     }
   }
 
