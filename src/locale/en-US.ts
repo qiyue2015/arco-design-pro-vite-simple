e09659d22bbfb37dcd86bbe176a2a0449077ddfa
白开水浇花 <fengqiyue@gmail.com>
2025-04-17 15:13:45 +0800

    feat: 移除不再使用的工作台相关本地化文件和模拟数据

--- src/locale/en-US.ts
+++ src/locale/en-US.ts
@@ -1,12 +1,10 @@
 import localeMessageBox from '@/components/message-box/locale/en-US';
 import localeLogin from '@/views/login/locale/en-US';
-
-import localeWorkplace from '@/views/dashboard/workplace/locale/en-US';
-
 import localeSettings from './en-US/settings';
 
 export default {
   'menu.dashboard': 'Dashboard',
+  'menu.dashboard.workplace': 'Workplace',
   'menu.server.dashboard': 'Dashboard-Server',
   'menu.server.workplace': 'Workplace-Server',
   'menu.server.monitor': 'Monitor-Server',
@@ -25,5 +23,4 @@ export default {
   ...localeSettings,
   ...localeMessageBox,
   ...localeLogin,
-  ...localeWorkplace,
 };
--- src/locale/zh-CN.ts
+++ src/locale/zh-CN.ts
@@ -1,12 +1,11 @@
 import localeMessageBox from '@/components/message-box/locale/zh-CN';
 import localeLogin from '@/views/login/locale/zh-CN';
 
-import localeWorkplace from '@/views/dashboard/workplace/locale/zh-CN';
-
 import localeSettings from './zh-CN/settings';
 
 export default {
   'menu.dashboard': '仪表盘',
+  'menu.dashboard.workplace': '工作台',
   'menu.server.dashboard': '仪表盘-服务端',
   'menu.server.workplace': '工作台-服务端',
   'menu.server.monitor': '实时监控-服务端',
@@ -25,5 +24,4 @@ export default {
   ...localeSettings,
   ...localeMessageBox,
   ...localeLogin,
-  ...localeWorkplace,
 };
