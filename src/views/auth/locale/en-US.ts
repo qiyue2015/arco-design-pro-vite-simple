38d01128a290ef549cb3d2a7682277fbc71c2e01
白开水浇花 <fengqiyue@gmail.com>
2025-05-15 17:16:53 +0800

    feat: 实现用户注册和登录功能

--- src/views/auth/locale/en-US.ts
+++ src/views/auth/locale/en-US.ts
@@ -9,7 +9,7 @@ export default {
   'login.form.rememberPassword': 'Remember password',
   'login.form.forgetPassword': 'Forgot password',
   'login.form.login': 'login',
-  'login.form.register': 'register account',
+  'login.form.register': 'Register a new account',
   'login.banner.slogan1': 'Out-of-the-box high-quality template',
   'login.banner.subSlogan1': 'Rich page templates, covering most typical business scenarios',
   'login.banner.slogan2': 'Built-in solutions to common problems',
