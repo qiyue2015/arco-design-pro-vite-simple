9a0f2f5efc29f782f26163f6365a2da7aab66b5a
白开水浇花 <fengqiyue@gmail.com>
2026-01-10 02:01:42 +0800

    refactor(workplace): remove old workplace, add new dashboard/workplace structure

--- src/views/dashboard/workplace/components/guide.vue
+++ src/views/dashboard/workplace/components/guide.vue
@@ -8,9 +8,9 @@
   .guide {
     display: flex;
     min-width: 1000px;
-    overflow: hidden;
     border-radius: 8px;
-    opacity: 1;
     transition: all 0.2s linear;
+    overflow: hidden;
+    opacity: 1;
   }
 </style>
